05e36e00ecbbe3480fe440d379c2be61890effef
민경호 <1736s@naver.com>
2023-09-21 21:37:25 +0900

    fit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
+import { AppController } from './app/app.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
